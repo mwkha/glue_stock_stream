e2369f5adc50fa3fd9137eed320fc80e526ea178
mwkha <wassimkhan3@gmail.com>
2024-02-10 15:40:14 +1100

    Push csv + kafka setup

--- init.py
+++ init.py
@@ -1,1 +1,31 @@
+import pandas as pd
+from kafka import KafkaConsumer, KafkaProducer
+from time import sleep
+from json import dumps
+import json
+
+
+HOST = "localhost"
+PORT = "9092"
+KAFKA_SERVER = HOST + ":" + PORT
+
+producer = KafkaProducer(
+    bootstrap_servers = KAFKA_SERVER,
+    key_serializer = lambda v: json.dumps(v).encode("ascii"),
+    value_serializer = lambda v: json.dumps(v).encode("ascii")
+    )
+
+df = pd.read_csv("indexProcessed.csv")
+print(df.head())
+# Sample returns a random sample of items from an axis of object.
+# to_dict Convert the DataFrame to a dictionary, orient determines the type of the values of the dictionary.
+dict_record = df.sample(1).to_dict(orient = "records")
+print(dict_record)
+
+
+
+
+
+producer.send('stock_streamer', value = dict_record)
+producer.close()
 
---
+++ services.sh
@@ -0,0 +1,3 @@
+brew services $1 zookeeper
+brew services $1 kafka
+brew services 
